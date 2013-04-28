aefb9432d48d8171a22c6a79321a2b4544a3b443
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2013-04-29 00:07:28 +0700

    Get rid of hardcoded paths

--- sass/index.js
+++ sass/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var util = require('util'),
+var fs = require('fs'),
+    path = require('path'),
+    util = require('util'),
     yeoman = require('yeoman-generator');
-
 
 var Generator = module.exports = function Generator () {
     yeoman.generators.Base.apply(this, arguments);
@@ -18,15 +19,12 @@ Generator.prototype.sass = function sass () {
     this.remote('nDmitry', 'sass', function(err, remote) {
         if (err) { return cb(err); }
 
-        remote.directory('lib/', 'src/sass/lib/');
-        remote.copy('_common.scss', 'src/sass/_common.scss');
-        remote.copy('_layouts.scss', 'src/sass/_layouts.scss');
-        remote.copy('_print.scss', 'src/sass/_print.scss');
-        remote.copy('_screen.scss', 'src/sass/_screen.scss');
-        remote.copy('_shame.scss', 'src/sass/_shame.scss');
-        remote.copy('_vars.scss', 'src/sass/_vars.scss');
-        remote.copy('main.scss', 'src/sass/main.scss');
+        remote.directory('.', 'src/sass/');
 
         cb();
     });
+};
+
+Generator.prototype.remove = function remove () {
+    fs.unlinkSync(path.join(process.cwd(), 'src/sass/.gitignore'));
 };
