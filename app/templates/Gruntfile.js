dda4cb04e2242a479430fef388c443457b438d6c
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2013-09-04 15:12:08 +0700

    Set the build folder as base for Connect

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
                 options: {
                     hostname: '*',
                     port: 9001,
-                    base: './',
+                    base: './<%%= buildDir %>',
                     livereload: true
                 }
             }
