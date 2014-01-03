5f3c1089d14a45f4d908b7c8371f798eb478f883
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2014-01-03 13:50:39 +0700

    Refactor

--- app/index.js
+++ app/index.js
@@ -1,66 +1,65 @@
 'use strict';
 
 var path = require('path'),
-    util = require('util'),
-    yeoman = require('yeoman-generator');
+    yo = require('yeoman-generator');
 
 
-var AppGenerator = module.exports = function AppGenerator(arg, options) {
-    yeoman.generators.Base.apply(this, arguments);
+module.exports = yo.generators.Base.extend({
+    constructor: function(arg, options) {
+        yo.generators.Base.apply(this, arguments);
 
-    this.on('end', function() {
-        this.installDependencies({
-            skipInstall: options['skip-install']
+        this.on('end', function() {
+            this.installDependencies({
+                skipInstall: options['skip-install']
+            });
         });
-    });
 
-    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-    this.bwr = JSON.parse(this.readFileAsString(path.join(__dirname, '/templates/bowerrc')));
-};
+        this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+        this.bwr = JSON.parse(this.readFileAsString(path.join(__dirname, '/templates/bowerrc')));
+    },
 
-util.inherits(AppGenerator, yeoman.generators.NamedBase);
+    askFor: function() {
+        var cb = this.async();
 
-AppGenerator.prototype.askFor = function askFor() {
-    var cb = this.async();
-
-    var prompts = [
-        {
-            name: 'projectName',
-            message: 'Project Name',
-            default: path.basename(process.cwd())
-        },
-        {
-            name: 'lang',
-            message: 'Project Language',
-            default: 'ru'
-        }
-    ];
-
-    this.prompt(prompts, function(props) {
-        for (var prop in props) {
-            if (props.hasOwnProperty(prop)) {
-                this[prop] = props[prop];
+        var prompts = [
+            {
+                name: 'projectName',
+                message: 'Project Name',
+                default: path.basename(process.cwd())
+            },
+            {
+                name: 'lang',
+                message: 'Project Language',
+                default: 'ru'
             }
-        }
+        ];
 
-        cb();
-    }.bind(this));
+        this.prompt(prompts, function(props) {
+            for (var prop in props) {
+                if (props.hasOwnProperty(prop)) {
+                    this[prop] = props[prop];
+                }
+            }
 
-};
+            cb();
+        }.bind(this));
 
-AppGenerator.prototype.app = function() {
-    this.directory('src/', 'src/');
+    },
 
-    this.mkdir('src/fonts');
-    this.mkdir('src/img/sprites/2x');
+    scaffold: function() {
+        this.directory('src/', 'src/');
 
-    this.copy('_gitignore', '.gitignore');
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-    this.copy('bowerrc', '.bowerrc');
+        this.mkdir('src/fonts');
+        this.mkdir('src/img/sprites/2x');
 
-    this.template('_bower.json', 'bower.json');
-    this.template('_package.json', 'package.json');
-    this.template('_Gruntfile.js', 'Gruntfile.js');
-    this.template('_index.ejs', 'src/pages/index.ejs');
-};
+        this.copy('_gitignore', '.gitignore');
+        this.copy('editorconfig', '.editorconfig');
+        this.copy('jshintrc', '.jshintrc');
+        this.copy('bowerrc', '.bowerrc');
+
+        this.template('_bower.json', 'bower.json');
+        this.template('_package.json', 'package.json');
+        this.template('_Gruntfile.js', 'Gruntfile.js');
+        this.template('_index.ejs', 'src/pages/index.ejs');
+    }
+});
--- stylus/index.js
+++ stylus/index.js
@@ -1,58 +1,53 @@
 'use strict';
 
 var path = require('path'),
-    util = require('util'),
-    yeoman = require('yeoman-generator');
+    yo = require('yeoman-generator');
 
-var StylusGenerator = module.exports = function StylusGenerator() {
-    yeoman.generators.Base.apply(this, arguments);
-};
+module.exports = yo.generators.Base.extend({
+    askFor: function() {
+        var cb = this.async();
 
-util.inherits(StylusGenerator, yeoman.generators.NamedBase);
-
-StylusGenerator.prototype.askFor = function askFor() {
-    var cb = this.async();
-
-    var prompts = [
-        {
-            name: 'path',
-            message: 'Path',
-            default: 'src/stylus/'
-        }
-    ];
-
-    this.prompt(prompts, function(props) {
-        for (var prop in props) {
-            if (props.hasOwnProperty(prop)) {
-                this[prop] = props[prop];
+        var prompts = [
+            {
+                name: 'path',
+                message: 'Path',
+                default: 'src/stylus/'
             }
-        }
+        ];
 
-        cb();
-    }.bind(this));
-};
+        this.prompt(prompts, function(props) {
+            for (var prop in props) {
+                if (props.hasOwnProperty(prop)) {
+                    this[prop] = props[prop];
+                }
+            }
 
-StylusGenerator.prototype.stylus = function stylus() {
-    var cb = this.async();
-
-    this.log.info('Fetching Stylus lib...');
-
-    this.remote('nDmitry', 'stylus', function(err, remote) {
-        if (err) {
-            return cb(err);
-        }
-
-        remote.directory('lib/', path.join(this.path, 'lib/'));
-        remote.directory('partials/', path.join(this.path, 'partials/'));
-
-        var files = this.expandFiles('*.styl', {
-            cwd: remote.cachePath
-        });
-
-        files.map(function(filename) {
-            remote.copy(filename, path.join(this.path, filename));
+            cb();
         }.bind(this));
+    },
 
-        cb();
-    }.bind(this));
-};
+    scaffold: function() {
+        var cb = this.async();
+
+        this.log.info('Fetching Stylus lib...');
+
+        this.remote('nDmitry', 'stylus', function(err, remote) {
+            if (err) {
+                return cb(err);
+            }
+
+            remote.directory('lib/', path.join(this.path, 'lib/'));
+            remote.directory('partials/', path.join(this.path, 'partials/'));
+
+            var files = this.expandFiles('*.styl', {
+                cwd: remote.cachePath
+            });
+
+            files.map(function(filename) {
+                remote.copy(filename, path.join(this.path, filename));
+            }.bind(this));
+
+            cb();
+        }.bind(this));
+    }
+});
